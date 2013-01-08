7be9becaf25fd36cd71f19c55fa15c1c53e7be7e
etb <esben.baek@gmail.com>
2013-01-08 13:03:17 -0800

    Parse data frame back to bizarro REDCap format added

--- R pull and push to REDCap.R
+++ R pull and push to REDCap.R
@@ -39,8 +39,43 @@ data <- Rawdata
 
 ####################################################################
 ##                                                                ##
+##        Parse data frame back to bizarro REDCap format          ##
+##                                                                ##
+####################################################################
+## needs to by optimized
+
+some_magic <- function(df) {
+    ## Replace NA with "", converting column types as needed
+    df[] <- lapply(df, function(X) {
+                if(any(is.na(X))) {X[is.na(X)] <- ""; X} else {X}
+            })
+
+    ## Print integers in first column as 2-digit character strings
+    ## (DO NOTE: Hardwiring the number of printed digits here is probably
+    ## inadvisable, though needed to _exactly_ reconstitute RAW.API.) 
+    df[[1]] <- sprintf("%1.0f", df[[1]])
+
+    ## Separately build header and table body, then suture them together 
+    l1 <- paste(names(df), collapse=",")
+    l2 <- capture.output(write.table(df, sep=",", col.names=FALSE, 
+                                     row.names=FALSE))
+    out <- paste0(c(l1, l2, ""), collapse="\n")
+
+    ## Reattach attributes
+    att <- list("Content-Type" = structure(c("text/html", "utf-8"), 
+                .Names = c("", "charset")))
+    attributes(out) <- att
+    out
+}
+
+API.OUT <- some_magic(data)
+
+####################################################################
+##                                                                ##
 ##                  Push files to REDCap                          ##
 ##                                                                ##
+
+cat(postForm(REDcap.URL, data=API.OUT, token=RC01, content="record", type="flat", format="csv",returnFormat="csv", overwriteBehavior="overwrite", .opts=curlOptions(ssl.verifypeer=FALSE)))
 
 
 #this is how you push pdf files
