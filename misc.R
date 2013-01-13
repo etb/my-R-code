a8076b01150a57b3c462fb2270a6a664c9587dcf
etb <esben.baek@gmail.com>
2013-01-13 15:57:36 -0800

    commenly used code added

--- misc.R
+++ misc.R
@@ -1,1 +1,5 @@
-options(repos=c("http://cran.cnr.Berkeley.edu")) 
+options(repos=c("http://cran.cnr.Berkeley.edu"))
+
+# install.packages(c("psych", "qgraph", "foreign", "mvbutils", "diagram", "gtools", "gdata", "ggplot2", "reshape"))
+# install.packages("Hmisc", dependencies = TRUE)
+# update.packages()
