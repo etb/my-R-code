a12c58f5d4f9a65008ebbdd9a947b66e6e5f9f9b
etb <esben.baek@gmail.com>
2013-01-21 15:47:50 -0800

    added usefull stuff to my misc R file

--- misc.R
+++ misc.R
@@ -1,5 +1,12 @@
-options(repos=c("http://cran.cnr.Berkeley.edu"))
+# rm(list = ls(all = TRUE)) ## Clear workspace
+  sessionInfo()
+
+options(repos=c("http://cran.at.r-project.org/"))
+# chooseCRANmirror(graphics = FALSE)
 
 # install.packages(c("psych", "qgraph", "foreign", "mvbutils", "diagram", "gtools", "gdata", "ggplot2", "reshape"))
-# install.packages("Hmisc", dependencies = TRUE)
+install.packages("Hmisc", dependencies = TRUE)
 # update.packages()
+
+## List the Files in a Directory/Folder
+list.files(pattern = "*.txt")
